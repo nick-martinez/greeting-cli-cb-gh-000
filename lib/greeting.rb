b06d161fa5ae7ad7c03ebe5e52f5de3661cadaac
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-17 21:16:03 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -3,5 +3,5 @@ def greeting(name)
   Puts "Hello #{name}. It's nice to meet you."
 end
 
-puts "please enter your name to make this more personal"
+# puts "please enter your name to make this more personal"
 # name = gets.strip
