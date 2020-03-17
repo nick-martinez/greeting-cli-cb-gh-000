7d63d5b9c1c7df1d69681be902ee9d2f79a8cc42
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-17 21:17:33 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-  Puts "Hello #{name}. It's nice to meet you."
+  puts "Hello #{name}. It's nice to meet you."
 end
 
 # puts "please enter your name to make this more personal"
