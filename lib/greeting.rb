9d4fcf5acf6cf0fb046fffbf307abc8790c08dc3
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-17 21:07:44 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,9 @@
 # code the #greeting method here!
+def greeting(name)
+  Puts "Hello #{name}. It's nice to meet you."
+end
+
+puts "please enter your name to make this more personal"
+name = gets.strip
+
+greeting(name)
